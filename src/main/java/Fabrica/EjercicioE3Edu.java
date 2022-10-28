cda41619f36ffd00c79059985fa8c72f193fc6f5
Eduardo <eduardocorreaaranda10@gmail.com>
2022-10-28 13:24:33 +0200

    ejercicio final

--- src/main/java/Fabrica/EjercicioE3Edu.java
+++ src/main/java/Fabrica/EjercicioE3Edu.java
@@ -19,9 +19,10 @@ public class EjercicioE3Edu {
         String menu;
         String menu2;
         final double COSTE_MANO_DE_OBRE1 = 0.5, COSTE_MANO_DE_OBRE2 = 0.9;
+        final double COSTE_MINIMO_MATERIAPRIMA = 0.1, COSTE_MAXIMO_MATERIAPRIMA = 1;
         final double PORCENTAJE1 = 0.5, PORCENTAJE2 = 0.65;
-        final double MANO_DE_OBRA_M1_T1 = 0.15, MANO_DE_OBRA_P1_M2_T2 = 0.22;
         double numMateriaPrima;
+        double numManoObra;
         double costeProduccion;
         double precioVenta = 0;
         
@@ -39,7 +40,8 @@ public class EjercicioE3Edu {
             do {  
                 
                 menu2 = JOptionPane.showInputDialog("""
-                                                           
+                                       
+                                         INTRODUCE EL CÓDIGO DEL PRODUCTO            
                                        ------------------------------------
                                                 PRODUCTOS
                                             1ºMantecados de Limón (M1)
@@ -47,23 +49,35 @@ public class EjercicioE3Edu {
                                             3ºTurrón de chocolate (T1)
                                             4ºTurrón clásica (T2)
                                             5ºMazapanes (M2)
+                                         --------------------------------
+                                             PARA TERMINAR EL PROGRAMA
+                                             INTRODUZCA 'SALIR'
+                                         --------------------------------           
                                        -------------------------------------  
                                                            """).toUpperCase();
-                
-                switch(menu2){
-                    case "1","3", "M1", "T1" :
-                        
-                        numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
+               
+                if(menu2.equals("M1")|| menu2.equals("P1")
+                       || menu2.equals("M2")){
+                   
+                   numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
                           ("Introduce la materia prima a utilizar: "));
                         
-                        while(numMateriaPrima <= COSTE_MANO_DE_OBRE1 && numMateriaPrima >= COSTE_MANO_DE_OBRE2){
+                        while(numMateriaPrima <= COSTE_MINIMO_MATERIAPRIMA && numMateriaPrima >= COSTE_MAXIMO_MATERIAPRIMA){
                               numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
                           ("Introduce la materia prima a utilizar: "));
                         }
-                        costeProduccion = numMateriaPrima + MANO_DE_OBRA_M1_T1;
+                        numManoObra = Double.parseDouble(JOptionPane.showInputDialog
+                          ("Introduce la mano de obra a utilizar: "));
+                        
+                         while(numManoObra <= COSTE_MANO_DE_OBRE1 && numManoObra >= COSTE_MANO_DE_OBRE2){
+                              numManoObra = Double.parseDouble(JOptionPane.showInputDialog
+                          ("Introduce la mano de obra a utilizar: "));
+                        }
+
+                        costeProduccion = numMateriaPrima + numManoObra;
                         precioVenta = costeProduccion + (costeProduccion * PORCENTAJE1);
                         
-                        JOptionPane.showMessageDialog(null, """
+                         JOptionPane.showMessageDialog(null, """
                                                             El coste de produccion es de %.2f € y el precio de venta es de
                                                             %.2f €.
                                                             """.formatted(costeProduccion, precioVenta));
@@ -73,38 +87,44 @@ public class EjercicioE3Edu {
                         }else{
                             JOptionPane.showMessageDialog(null,"No hay beneficios ");
                         }
-                      
-                     break;
-                    case "2", "4", "5", "P1", "T2", "M2" :
-                        
-                        numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
+               } 
+                 if(menu2.equals("T1")|| menu2.equals("T2")){
+                   
+                   numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
                           ("Introduce la materia prima a utilizar: "));
                         
-                        while(numMateriaPrima <= COSTE_MANO_DE_OBRE1 && numMateriaPrima >= COSTE_MANO_DE_OBRE2){
-                           numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
+                        while(numMateriaPrima <= COSTE_MINIMO_MATERIAPRIMA && numMateriaPrima >= COSTE_MAXIMO_MATERIAPRIMA){
+                              numMateriaPrima = Double.parseDouble(JOptionPane.showInputDialog
                           ("Introduce la materia prima a utilizar: "));
                         }
-                        costeProduccion = numMateriaPrima + MANO_DE_OBRA_P1_M2_T2;
+                        numManoObra = Double.parseDouble(JOptionPane.showInputDialog
+                          ("Introduce la mano de obra a utilizar: "));
+                        
+                         while(numManoObra <= COSTE_MANO_DE_OBRE1 && numManoObra >= COSTE_MANO_DE_OBRE2){
+                              numManoObra = Double.parseDouble(JOptionPane.showInputDialog
+                          ("Introduce la mano de obra a utilizar: "));
+                        }
+
+                        costeProduccion = numMateriaPrima + numManoObra;
                         precioVenta = costeProduccion + (costeProduccion * PORCENTAJE2);
                         
-                        JOptionPane.showMessageDialog(null, """
+                         JOptionPane.showMessageDialog(null, """
                                                             El coste de produccion es de %.2f € y el precio de venta es de
                                                             %.2f €.
                                                             """.formatted(costeProduccion, precioVenta));
                         
-                        if(precioVenta >= 2500){
-                           JOptionPane.showMessageDialog(null,"Hay beneficios ");
+                         if(precioVenta >= 2500){
+                          JOptionPane.showMessageDialog(null,"Hay beneficios ");
                         }else{
                             JOptionPane.showMessageDialog(null,"No hay beneficios ");
                         }
-                      
-                     break;
-                    default :
-                }
-            
+               } 
+
             }while(!(menu2.equalsIgnoreCase("salir")));
             
                 break;
+                case "2" :
+                    JOptionPane.showMessageDialog(null, "Para terminar el programa introduce 'Salir'");
                 default :        
             }
             
